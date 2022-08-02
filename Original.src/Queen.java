1d86f98647509e2e6588f73ae97f60903eac96ee
Sebastian Asis <sasisromero10@gmail.com>
2022-08-02 01:37:16 -0500

    add new name of variables

--- Original.src/Queen.java
+++ Original.src/Queen.java
@@ -35,24 +35,24 @@ public class Queen
      */
     @Override
     protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
-        ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 8 );
-        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 8 );
-        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 8 );
-        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 8 );
-        ArrayList<String> northMoves = calculateNorthMoves( board, 8 );
-        ArrayList<String> southMoves = calculateSouthMoves( board, 8 );
-        ArrayList<String> eastMoves = calculateEastMoves( board, 8 );
-        ArrayList<String> westMoves = calculateWestMoves( board, 8 );
-        ArrayList<String> allMoves = new ArrayList<String>();
-        allMoves.addAll( northEastMoves );
-        allMoves.addAll( northWestMoves );
-        allMoves.addAll( southWestMoves );
-        allMoves.addAll( southEastMoves );
-        allMoves.addAll( northMoves );
-        allMoves.addAll( southMoves );
-        allMoves.addAll( westMoves );
-        allMoves.addAll( eastMoves );
-        return allMoves;
+        ArrayList<String> NEMoves = calculateNEMoves( board, 8 );
+        ArrayList<String> NWMoves = calculateNWMoves( board, 8 );
+        ArrayList<String> SEMoves = calculateSEMoves( board, 8 );
+        ArrayList<String> SWMoves = calculateSWMoves( board, 8 );
+        ArrayList<String> NMoves = calculateNMoves( board, 8 );
+        ArrayList<String> SMoves = calculateSMoves( board, 8 );
+        ArrayList<String> EMoves = calculateEMoves( board, 8 );
+        ArrayList<String> WMoves = calculateWMoves( board, 8 );
+        ArrayList<String> totalMoves = new ArrayList<String>();
+        totalMoves.addAll( NEMoves );
+        totalMoves.addAll( NWMoves );
+        totalMoves.addAll( SWMoves );
+        totalMoves.addAll( SEMoves );
+        totalMoves.addAll( NMoves );
+        totalMoves.addAll( SMoves );
+        totalMoves.addAll( WMoves );
+        totalMoves.addAll( EMoves );
+        return totalMoves;
     }
     /**
      * Creates an icon for this piece depending on the piece's color.
