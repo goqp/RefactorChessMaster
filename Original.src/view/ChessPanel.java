d108af15af31d9037e27f86dd2026818ac94f11c
Edson <esalvador032@gmail.com>
2022-12-01 17:56:01 -0500

    Implement mvc structure

---
+++ Original.src/view/ChessPanel.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import javax.swing.*;
+// -------------------------------------------------------------------------
+/**
+ * The main panel of the Chess game.
+ * 
+ * @author Ben Katz (bakatz)
+ * @author Myles David II (davidmm2)
+ * @author Danielle Bushrow (dbushrow)
+ * @version 2010.11.17
+ */
+public class ChessPanel
+    extends JPanel{
+    private ChessMenuBar    menuBar;
+    private ChessGameBoard  gameBoard;
+    private ChessGameLog    gameLog;
+    private ChessGraveyard  playerOneGraveyard;
+    private ChessGraveyard  playerTwoGraveyard;
+    private ChessGameEngine gameEngine;
+    // ----------------------------------------------------------
+    /**
+     * Create a new ChessPanel object.
+     */
+    public ChessPanel(){
+        this.setLayout( new BorderLayout() );
+        menuBar = new ChessMenuBar();
+        gameBoard = new ChessGameBoard();
+        gameLog = new ChessGameLog();
+        playerOneGraveyard = new ChessGraveyard( "Player 1's graveyard" );
+        playerTwoGraveyard = new ChessGraveyard( "Player 2's graveyard" );
+        this.add( menuBar, BorderLayout.NORTH );
+        this.add( gameBoard, BorderLayout.CENTER );
+        this.add( gameLog, BorderLayout.SOUTH );
+        this.add( playerOneGraveyard, BorderLayout.WEST );
+        this.add( playerTwoGraveyard, BorderLayout.EAST );
+        this.setPreferredSize( new Dimension( 800, 600 ) );
+        gameEngine = new ChessGameEngine( gameBoard ); // start the game
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the logger object for use in other classes.
+     * 
+     * @return ChessGameLog the ChessGameLog object
+     */
+    public ChessGameLog getGameLog(){
+        return gameLog;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the board object for use in other classes.
+     * 
+     * @return ChessGameBoard the ChessGameBoard object
+     */
+    public ChessGameBoard getGameBoard(){
+        return gameBoard;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the game engine object for use in other classes
+     * 
+     * @return ChessGameEngine the ChessGameEngine object
+     */
+    public ChessGameEngine getGameEngine(){
+        return gameEngine;
+    }
+    // ----------------------------------------------------------
+    /**
+     * Gets the appropriate graveyard object for use in other classes.
+     * 
+     * @param whichPlayer
+     *            the number of the player (1 or 2)
+     * @return ChessGraveyard the graveyard requested
+     */
+    public ChessGraveyard getGraveyard( int whichPlayer ){
+        if ( whichPlayer == 1 ){
+            return playerOneGraveyard;
+        }
+        else if ( whichPlayer == 2 ){
+            return playerTwoGraveyard;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
